59309ecc1f621a4a3c6e3c93d1370d3eb4e3b295
tabrezsaqib <mohamedsaqib1996@gmail.com>
2023-01-21 13:32:51 -0400

    new feature - card added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  Card from "./components/cards/Card";
 
 function App() {
   return (
+    <>
     <h1>Welcome to My Pizza</h1>
+    <Card/>
+    </>
   );
 }
 
---
+++ src/components/cards/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Card.css";
+
+function Card(){
+    return(
+        <div className="card">
+      <div className="cardImg">
+        <img src="https://images.pexels.com/photos/22687/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=400" alt="No Image found"/>
+      </div>
+      <div className="cardContent">
+        <h2>Trail</h2>
+        <p>
+          This trail flourishes during summer with flowers freshly sprung along both the sides of the path.
+        </p>
+      </div>
+    </div>
+    );
+
+}
+
+export default Card;
